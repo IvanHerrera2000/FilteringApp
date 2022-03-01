86b33fdcf986a46e15f503e851c0c9b5c3381205
unknown <ivanalejandroherrera2000@gmail.com>
2022-03-01 17:02:03 -0300

    Setting up global error handler

--- backend/controllers/bootcampControllers.js
+++ backend/controllers/bootcampControllers.js
@@ -1,21 +1,23 @@
 const Bootcamp = require("../models/Bootcamp");
+const asyncHandler = require("../middleware/asyncHandler");
+const ErrorResponse = require("../utils/errorResponse");
 
-exports.getAllBootcamps = async (req, res, next) => {
+exports.getAllBootcamps = asyncHandler(async (req, res, next) => {
   const bootcamps = await Bootcamp.find();
   res.status(200).json({
     success: true,
     data: bootcamps,
   });
-};
+});
 
-exports.createNewBootcamp = async (req, res, next) => {
+exports.createNewBootcamp = asyncHandler(async (req, res, next) => {
   res.send("Create new bootcamp route");
-};
+});
 
-exports.updateBootcampById = async (req, res, next) => {
+exports.updateBootcampById = asyncHandler(async (req, res, next) => {
   res.send("Update a bootcamp by id route");
-};
+});
 
-exports.deleteBootcampById = async (req, res, next) => {
+exports.deleteBootcampById = asyncHandler(async (req, res, next) => {
   res.send("Delete a bootcamp by id route");
-};
+});
