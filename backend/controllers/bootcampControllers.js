87708dec72eb0de6eac1fc43d46db13fc354d733
unknown <ivanalejandroherrera2000@gmail.com>
2022-03-02 16:16:34 -0300

    Handling sorting on backend

--- backend/controllers/bootcampControllers.js
+++ backend/controllers/bootcampControllers.js
@@ -5,11 +5,23 @@ const ErrorResponse = require("../utils/errorResponse");
 exports.getAllBootcamps = asyncHandler(async (req, res, next) => {
   let query;
 
+  let uiValues = {
+    filtering: {},
+    sorting: {},
+  };
+
   const reqQuery = { ...req.query };
 
   const removeFields = ["sort"];
 
   removeFields.forEach((val) => delete reqQuery[val]);
+
+  const filterKeys = Object.keys(reqQuery);
+  const filterValues = Object.values(reqQuery);
+
+  filterKeys.forEach(
+    (val, idx) => (uiValues.filtering[val] = filterValues[idx])
+  );
 
   let queryStr = JSON.stringify(reqQuery);
 
@@ -18,16 +30,55 @@ exports.getAllBootcamps = asyncHandler(async (req, res, next) => {
     (match) => `$${match}`
   );
 
-  const bootcamps = await Bootcamp.find(JSON.parse(queryStr));
+  query = Bootcamp.find(JSON.parse(queryStr));
+
+  if (req.query.sort) {
+    const sortByArr = req.query.sort.split(",");
+
+    sortByArr.forEach((val) => {
+      let order;
+
+      if (val[0] === "-") {
+        order = "descending";
+      } else {
+        order = "ascending";
+      }
+
+      uiValues.sorting[val.replace("-", "")] = order;
+    });
+
+    const sortByStr = sortByArr.join(" ");
+
+    query = query.sort(sortByStr);
+  } else {
+    query = query.sort("-price");
+  }
+
+  const bootcamps = await query;
+
+  const maxPrice = await Bootcamp.find()
+    .sort({ price: -1 })
+    .limit(1)
+    .select("-_id price");
+
+  const minPrice = await Bootcamp.find()
+    .sort({ price: 1 })
+    .limit(1)
+    .select("-_id price");
+
+  uiValues.maxPrice = maxPrice[0].price;
+  uiValues.minPrice = minPrice[0].price;
 
   res.status(200).json({
     success: true,
     data: bootcamps,
+    uiValues,
   });
 });
 
 exports.createNewBootcamp = asyncHandler(async (req, res, next) => {
   const bootcamp = await Bootcamp.create(req.body);
+
   res.status(201).json({
     success: true,
     data: bootcamp,
@@ -36,15 +87,18 @@ exports.createNewBootcamp = asyncHandler(async (req, res, next) => {
 
 exports.updateBootcampById = asyncHandler(async (req, res, next) => {
   let bootcamp = await Bootcamp.findById(req.params.id);
+
   if (!bootcamp) {
     return next(
       new ErrorResponse(`Bootcamp with id ${req.params.id} was not found`, 404)
     );
   }
+
   bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
     runValidators: true,
   });
+
   res.status(201).json({
     success: true,
     data: bootcamp,
@@ -53,12 +107,15 @@ exports.updateBootcampById = asyncHandler(async (req, res, next) => {
 
 exports.deleteBootcampById = asyncHandler(async (req, res, next) => {
   let bootcamp = await Bootcamp.findById(req.params.id);
+
   if (!bootcamp) {
     return next(
       new ErrorResponse(`Bootcamp with id ${req.params.id} was not found`, 404)
     );
   }
+
   await bootcamp.remove();
+
   res.status(200).json({
     success: true,
     data: {},
